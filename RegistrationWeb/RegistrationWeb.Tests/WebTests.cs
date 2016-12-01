6ca859eb900647609bcf19c599e18dc7e0cc6e1a
raykonopka <konopka.ray@gmail.com>
2016-12-01 13:31:17 -0500

    add tests

---
+++ RegistrationWeb/RegistrationWeb.Tests/WebTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RegistrationWeb.Tests
+{
+    public class WebTests
+    {
+
+        [Fact]
+        public void Test_LoadIndexPage()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var actual = httpClient.GetAsync("http://34.193.163.157/registration-app-ui/").Result;
+
+            if (actual.IsSuccessStatusCode)
+            {
+                Debug.WriteLine("Index Page Sucessfully Loaded.");
+            }
+            else
+            {
+                Debug.WriteLine("Index Page Failed To Load.");
+            }
+
+            Assert.True(actual.IsSuccessStatusCode);
+        }
+
+    }
+}
